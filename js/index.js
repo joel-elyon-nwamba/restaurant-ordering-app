513727e3d97564e34733c9602e7607f411878e6d
joel-nwamba <joel.elyon.nwamba@gmail.com>
2026-05-18 15:14:19 -0400

    All buttons interacting when console loging

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 import { menuArray } from "./data.js";
 const menuOption = document.querySelector(".menu");
 
-
-
 function render() {
     menuArray.forEach((item) => {
         const divItemMenu = document.createElement("div");
@@ -13,9 +11,9 @@ function render() {
                 <h3>${item.name}</h3>
                 <p>${item.ingredients}</p>
                 <p>$${item.price}</p>
-                <button class="order-btn" data-id=${item.id}>+</button>
+                <button class="order-btn" data-id="${item.id}">+</button>
             </div>
-        
+
         `
         menuOption.appendChild(divItemMenu)
     });
@@ -23,14 +21,21 @@ function render() {
 
 render();
 
-// Button selector 
-
 const buttons = document.querySelectorAll("button");
 
+// HandleClick that will display all the information once thwe user clicks on the button
+function handleClick(orderId) {
+    const orderIdMenu = menuArray.filter((item) => {
+        return item.id === parseInt(orderId)
+    })[0];
+    console.log(orderIdMenu);
+}
+
+// Button selector
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
         if(e.target.dataset.id){
-            console.log(e.target.dataset.id)
+            handleClick(e.target.dataset.id);
         }
     })
 })
